fcdcf4b6fab2745f35935695da50fc5d6babd64f
mloherr <herraizmaria08@gmail.com>
2024-04-18 18:11:43 +0200

    Created new endpoints for search one book by id, and for add a new book

--- src/index.js
+++ src/index.js
@@ -37,3 +37,56 @@ server.get('/books', async (req, res) => {
     results: result,
   });
 });
+
+server.get('/books/:id', async (req, res) => {
+  const idBook = req.params.id;
+
+  const connection = await getDBConnection();
+  const querySQL = 'SELECT * FROM books WHERE id_books = ?';
+  const [result] = await connection.query(querySQL, [idBook]);
+
+  connection.end();
+
+  if (result.length === 0) {
+    res.status(404).json({
+      success: false,
+      error: "There's not an element with that id",
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      result: result,
+    });
+  }
+});
+
+server.post('/newbook', async (req, res) => {
+  try {
+    const data = req.body;
+    const { title, summary, author, pages, image, price, genre } = data;
+
+    const connection = await getDBConnection();
+    const querySQL =
+      'INSERT INTO books(title, summary, name_author, pages, image, price, fk_genre) VALUES(?, ?, ?, ?, ?, ?, ?)';
+
+    const [resultInsert] = await connection.query(querySQL, [
+      title,
+      summary,
+      author,
+      pages,
+      image,
+      price,
+      genre,
+    ]);
+
+    res.status(201).json({
+      success: true,
+      id: resultInsert.insertId,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
